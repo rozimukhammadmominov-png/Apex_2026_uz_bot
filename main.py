eea3f4b6fcdd35025002ebf4e168de0074518186
rozimukhammadmominov-png <rozimukhammadmominov@gmail.com>
2026-07-26 14:30:49 +0500

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,26 @@
+import asyncio
+from aiogram import Bot, Dispatcher
+from aiogram.filters import CommandStart
+from aiogram.types import Message
 
+BOT_TOKEN = "8905789957:AAEnR-Uww8DVqeulXzGoTR3O6uStmXuiDOE"
+
+bot = Bot(BOT_TOKEN)
+dp = Dispatcher()
+
+
+@dp.message(CommandStart())
+async def start(message: Message):
+    await message.answer(
+        "👋 Assalomu alaykum!\n\n"
+        "Apex Insurance botiga xush kelibsiz."
+    )
+
+
+async def main():
+    print("✅ Bot ishga tushdi")
+    await dp.start_polling(bot)
+
+
+if __name__ == "__main__":
+    asyncio.run(main())
