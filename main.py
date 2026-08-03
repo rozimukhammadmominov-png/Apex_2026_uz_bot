63ccb4e9884ea807aa0aed4d6216beaa2dc2e835
rozimukhammadmominov-png <rozimukhammadmominov@gmail.com>
2026-08-03 10:06:59 +0500

    Update main.py

--- main.py
+++ main.py
@@ -51,3 +51,10 @@ async def start(message: Message):
         "Kerakli xizmatni tanlang:",
         reply_markup=menu
 )
+
+async def main():
+    print("✅ Bot ishga tushdi")
+    await dp.start_polling(bot)
+
+if __name__ == "__main__":
+    asyncio.run(main())
